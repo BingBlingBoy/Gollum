504b9929ae653e1d24b0af9d27770d551e0205f2
BingBlingBoy <ckeiranbarnes@gmail.com>
2024-05-29 13:47:28 +0100

    added more endpoints

--- server/src/server.ts
+++ server/src/server.ts
@@ -5,7 +5,7 @@ import dotenv from "dotenv"
 dotenv.config();
 
 const app = express();
-const port = 3000
+const port = process.env.PORT 
 
 app.use(cors({
     origin : [
@@ -31,7 +31,51 @@ const getSpotifyAccessToken = async () => {
     return data
 }
 
-app.get('/spotify', async (req, res) => {
+const getNewReleases = async (token: string) => {
+    const result = await fetch(`https://api.spotify.com/v1/browse/new-releases`, {
+        method: 'GET',
+        headers: { 'Authorization' : 'Bearer ' + token}
+    });
+
+    const data = await result.json()
+    return data
+}
+
+const search = async (token: string, type: string, query: string) => {
+    console.log(token)
+    const searchParameters = {
+        method: 'GET',
+        headers: {
+            'Authorization': `Bearer ${token}`
+        }
+    }
+    // const result = await fetch('https://api.spotify.com/v1/search?' +`type=${type}&` + `q=${query}` , searchParameters)
+
+    try {
+        const response = await fetch('https://api.spotify.com/v1/search?q=' + query + `&type=${type}`, searchParameters)
+        if (!response.ok) {
+            throw new Error("Couldn't fetch data")
+        }
+        const data = await response.json()
+        return data
+    } catch (error) {
+        return error
+    }
+    // const result = await fetch(`https://api.spotify.com/v1/search?q=kendrick&type=album`, {
+    //     method: 'GET',
+    //     headers: {
+    //         'content-type' : 'application/json', 
+    //         'Authorization' : 'Bearer ' + token
+    //     }
+    // })
+
+    // console.log(result)
+    //
+    // const data = await result.json()
+    // return data
+}
+
+app.get('/spotify/getToken', async (req, res) => {
     try {
         const token = await getSpotifyAccessToken();
         res.json({
@@ -43,6 +87,32 @@ app.get('/spotify', async (req, res) => {
     }
 });
 
+app.get('/spotify/newReleases', async (req, res) => {
+    try {
+        const token = await getSpotifyAccessToken()
+        const newAlbumReleases = await getNewReleases(token.access_token);
+        res.json({
+            albums: newAlbumReleases.albums
+        })
+    } catch (error) {
+        res.status(500).json({error: 'Failed to get new Released albums'})
+    }
+})
+
+app.get('/spotify/search/:type/:query', async (req, res) => {
+    try {
+        const type = req.params.type
+        const query = req.params.query
+
+        const token = await getSpotifyAccessToken()
+        const searchResults = await search(token.access_token, type, query);
+        res.json({
+            type: searchResults
+        })
+    } catch (error) {
+        res.status(500).json({error: 'Failed to get new Released albums'})
+    }
+})
 app.listen(port, () => {
     console.log(`Server goes on http://localhost:${port}`)
 })
