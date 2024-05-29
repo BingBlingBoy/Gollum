f7fbc92e74c81a0828fb2a996d617440930db7e4
BingBlingBoy <ckeiranbarnes@gmail.com>
2024-05-29 16:38:18 +0100

    displayed newreleases

--- client/src/components/Newreleases.tsx
+++ client/src/components/Newreleases.tsx
@@ -2,20 +2,42 @@ import { useQuery } from "@tanstack/react-query"
 
 const Newreleases = () => {
 
-  const retrieveAccessToken = async () => {
+  interface albums {
+    id: string
+    name: string
+    artists: Artists[]
+    images: Image[]
+  }
+
+  interface Artists {
+    name: string,
+  }
+
+  interface artist {
+    name: string
+  }
+
+  interface Image {
+    height?: number
+    url: string
+    width?: number
+  }
+
+  const retrieveNewAlbumReleases = async () => {
     try {
-      const response = await fetch('http://localhost:3000/spotify/getToken')
+      const response = await fetch('http://localhost:3000/spotify/newReleases')
       const data = await response.json()
-      return data
+      return data.albums.items
     } catch (error) {
       return error
     }
   }
 
-  const { data: token, error, isLoading } = useQuery({
-    queryKey: ['spotifyToken'],
-    queryFn: retrieveAccessToken,
+  const {data: newReleases, error, isLoading} = useQuery({
+    queryKey: ['newReleases'],
+    queryFn: retrieveNewAlbumReleases
   })
+
 
   if (isLoading) {
     return <div>Loading...</div>;
@@ -26,7 +48,21 @@ const Newreleases = () => {
   }
 
   const content = (
-    <h1 className="text-3xl font-bold">{token?.access_token}</h1>
+    <>
+      <div className="p-10 flex items-center justify-center bg-best-gray">
+        <div className="grid grid-cols-3 gap-x-4">
+          {newReleases.map((data: albums, i:number) => (
+            <div className="relative flex justify-center flex-col py-4" key={i}>
+              <img src={data.images[0].url} alt="album picture" />
+              <p className="absolute bottom-10 left-4 font-semibold py-2 text-white drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)] text-xl">{data.name}</p>
+              {data.artists.map((d: artist, j: number) => (
+                <p className="absolute bottom-7 left-4 text-white drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)] text-md" key={j}>{d.name}</p>
+              ))}
+            </div>
+          ))}
+        </div>
+      </div>
+    </>
   )
 
   return content
