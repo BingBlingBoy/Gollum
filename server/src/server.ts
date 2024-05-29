d14f249e026e17726272ac336d34a33730b940d2
BingBlingBoy <ckeiranbarnes@gmail.com>
2024-05-29 02:28:39 +0100

    Getting Spotify Token

--- server/src/server.ts
+++ server/src/server.ts
@@ -13,6 +13,36 @@ app.use(cors({
     ]
 }))
 
+const clientId = process.env.CLIENT_ID
+const clientSecret = process.env.CLIENT_SECRET
+
+const getSpotifyAccessToken = async () => {
+
+    const result = await fetch("https://accounts.spotify.com/api/token", {
+        method: 'POST',
+        headers: {
+            'content-type': 'application/x-www-form-urlencoded',
+            'Authorization': 'Basic ' + (Buffer.from(clientId + ':' + clientSecret).toString('base64'))
+        },
+        body: 'grant_type=client_credentials'
+    })
+
+    const data = await result.json()
+    return data
+}
+
+app.get('/spotify', async (req, res) => {
+    try {
+        const token = await getSpotifyAccessToken();
+        res.json({
+            access_token: token.access_token,
+            expires_in: token.expires_in
+        });
+    } catch (error) {
+        res.status(500).json({ error: 'Failed to retrieve access token' });
+    }
+});
+
 app.listen(port, () => {
     console.log(`Server goes on http://localhost:${port}`)
 })
