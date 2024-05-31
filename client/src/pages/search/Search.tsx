2362e4b2fdbda54261ac399cde8276020857202f
BingBlingBoy <ckeiranbarnes@gmail.com>
2024-05-31 15:01:11 +0100

    added Artist to search menu

--- client/src/pages/search/Search.tsx
+++ client/src/pages/search/Search.tsx
@@ -83,22 +83,35 @@ const Search = () => {
         }
     }
 
+    const retrieveArtistSearchResult = async () => {
+        setSelectedCategory("Artist")
+        try {
+            const result = await fetch(`http://localhost:3000/spotify/search/${state.query}/artist`)
+            const data = await result.json()
+            return data
+        } catch (error) {
+            throw new Error("Could not search")
+        }
+    }
+
+    const {data: searchArtistResults, isFetching: fetchingArtists, refetch: artistRefetch} = useQuery({
+        queryKey: ['aristSearchResults'],
+        queryFn: retrieveArtistSearchResult,
+    })
+
     const {data: searchTrackResults, isFetching: fetchingTracks, refetch: trackRefetch} = useQuery({
         queryKey: ['trackSearchResults'],
         queryFn: retrieveTrackSearchResult,
-        refetchOnWindowFocus: true
     })
 
     const {data: searchAlbumResults, isFetching: fetchingAlbums, refetch: albumRefetch} = useQuery({
         queryKey: ['albumSearchResults'],
         queryFn: retrieveAlbumSearchResult,
-        refetchOnWindowFocus: true
     })
 
     const {data: searchResults, isFetching: fetchingEverything, refetch: defaultRefetch} = useQuery({
         queryKey: ['defaultSearchResults'],
         queryFn: retrieveDefaultSearchResult,
-        refetchOnWindowFocus: true
     })
 
     useEffect(() => {
@@ -111,8 +124,11 @@ const Search = () => {
         if (selectedCategory == "Track") {
             trackRefetch()
         }
+        if (selectedCategory == "Artist") {
+            artistRefetch()
+        }
         console.log(selectedCategory)
-    },[defaultRefetch, albumRefetch, trackRefetch, selectedCategory, state.query])
+    },[defaultRefetch, albumRefetch, trackRefetch, artistRefetch,selectedCategory, state.query])
 
     const content = (
         <>
@@ -122,6 +138,7 @@ const Search = () => {
                     <p className="text-2xl font-bold">Search Results for "{state.query}"</p>
                     <ul className="flex gap-12 pt-4">
                         <button onClick={retrieveDefaultSearchResult} className={`p-2 ${selectedCategory === "Everything" ? "border-b-2 border-accent" : ""}`}>Everything</button>
+                        <button onClick={retrieveArtistSearchResult} className={`p-2 ${selectedCategory === "Artist" ? "border-b-2 border-accent" : ""}`}>Artist</button>
                         <button onClick={retrieveAlbumSearchResult} className={`p-2 ${selectedCategory === "Album" ? "border-b-2 border-accent" : ""}`}>Album</button>
                         <button onClick={retrieveTrackSearchResult} className={`p-2 ${selectedCategory === "Track" ? "border-b-2 border-accent" : ""}`}>Track</button>
                     </ul>
@@ -230,6 +247,31 @@ const Search = () => {
                                                     <div className="pl-4">
                                                         <ol><a className="text-md font-bold text-black" href={data.external_urls.spotify}>{data.name}</a></ol>
                                                         <p className="text-black text-sm"><a href={data.artists[0].external_urls.spotify}>{data.artists[0].name}</a></p>
+                                                    </div>
+                                                </div>
+                                            ))}    
+                                        </div>
+                                    </>
+                                }
+                            </div>
+                        </div>
+                }
+                {
+                    (selectedCategory == "Artist" && !fetchingArtists) &&
+                        <div className="mt-16">
+                            <h1 className="text-3xl mb-2">Artists</h1>
+                            <div>
+                                {
+                                    <>
+                                        <div className="grid grid-cols-2">
+                                            {searchArtistResults.type.artists.items.map((data:Artists, i: number) => (
+                                                <div className="flex flex-row gap-2 py-4">
+                                                    <div className="max-w-20 max-h-20 bg-gradient-to-t from-gray-300 to-white" key={i}>
+                                                        <img className="w-full h-full" src={data.images.length !== 0 ? data.images[0].url : profile_page} alt="Artist Picture"/>
+                                                    </div>
+                                                    <div className="pl-4">
+                                                        <ol><a className="text-md font-bold text-black" href={data.external_urls.spotify}>{data.name}</a></ol>
+                                                        <p className="text-black text-sm"><a href={data.external_urls.spotify}>{data.name}</a></p>
                                                     </div>
                                                 </div>
                                             ))}    
